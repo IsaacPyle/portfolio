b3c054e46ac70dd6b42205e1458d96ecd8d7aa30
IsaacPyle <isaac.a.pyle@gmail.com>
2023-07-25 10:46:09 -0400

    working on getting harp to function properly, moved to a src folder and compiled

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+if (document.getElementById('my-work-link')) {
+  document.getElementById('my-work-link').addEventListener('click', () => {
+    document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
+  })
+}
+
+// $(".change" ).on("click", function() {
+//     if( $("body").hasClass("dark")) {
+//         $("body").removeClass("dark");
+//         $("a").removeClass("dark-with-border");
+//     } else {
+//         $("body").addClass("dark");
+//         $("a").addClass("dark-with-border");
+//     }
+// });
