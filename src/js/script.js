69d98f8333b2ec2161be3b6d38814068bcd22042
IsaacPyle <isaac.a.pyle@gmail.com>
2023-07-26 11:01:54 -0400

    More slight changes, added more pictures, working on portfolio project page

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 if (document.getElementById('my-work-link')) {
   document.getElementById('my-work-link').addEventListener('click', () => {
-    document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
+    document.getElementById('my-work-section-anchor').scrollIntoView({behavior: "smooth"})
   })
 }
 
